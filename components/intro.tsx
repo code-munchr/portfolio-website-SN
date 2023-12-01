9d5af49c20c2adf33bcf4087b9019f08bfe3696a
Hak~ <haroon1azhar@gmail.com>
2023-12-01 22:51:07 +0000

    changed3

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Sajad.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">Software Engineer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">web apps</span>. My work is primarily focused on{" "}
         <span className="underline">Javascript</span> But I'm also proficient with ROR and Django.
