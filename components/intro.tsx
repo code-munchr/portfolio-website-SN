8285beceb68edb6cab6765c664a98f9f200b920f
Hak~ <haroon1azhar@gmail.com>
2023-12-01 21:42:36 +0000

    changed1

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import profilePic from "@/public/HAK_porfolio.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,10 +32,10 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
-              width="192"
-              height="192"
+              src={profilePic}
+              alt="Personal portrait"
+              width="400"
+              height="400"
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
@@ -62,11 +63,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Sajad.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span className="italic">web apps</span>. My work is primarily focused on{" "}
+        <span className="underline">Javascript</span> But I'm also proficient with ROR and Django.
       </motion.h1>
 
       <motion.div
